50c4338a0be8f690c62ed0b58c408593849d9ee0
raul farl <raulfarllobell@gmail.com>
2021-11-06 12:27:34 +0100

    changes users controllers

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -98,7 +98,7 @@ UsuarioController.update = (req, res) => {
 const email = req.body.email;
 
 //Usuario.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
- Usuario.findByOneAndUpdate(email, req.body, { useFindAndModify: false })
+ Usuario.findEmailAndUpdate(email, req.body, { useFindAndModify: false })
     .then(data => {
       if (!data) {
         res.status(404).send({
